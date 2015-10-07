cb3da2bc86ebf7cf6a6770ab3cef1582c4b0064a
Sanjoy Das <sanjoy@playingwithpointers.com>
2015-10-06 23:44:26 -0700

    have test-euclid work in a release build

--- src/tests/test-euclid.cpp
+++ src/tests/test-euclid.cpp
@@ -5,20 +5,35 @@
 
 using namespace ray;
 
+#define CHECK(pred, msg) do {                                   \
+    if (!(pred)) {                                              \
+      std::cerr << #pred << " failed in "                       \
+                << __FILE__ << ":" << __LINE__ << std::endl;    \
+      std::cerr << msg << std::endl;                            \
+    }                                                           \
+  } while (false)
+
+#define CHECK0(pred) do {                                       \
+    if (!(pred)) {                                              \
+      std::cerr << #pred << " failed in "                       \
+                << __FILE__ << ":" << __LINE__ << std::endl;    \
+    }                                                           \
+  } while (false)
+
 static void test_cross_product() {
   Vector v;
   Vector vi = Vector::get_i();
   Vector vj = Vector::get_j();;
   Vector vk = Vector::get_k();;
 
-  assert(v.cross_product(v).is_zero() && "Should be!");
-  assert(vi.cross_product(vi).is_zero() && "Should be!");
-  assert(vj.cross_product(vj).is_zero() && "Should be!");
-  assert(vk.cross_product(vk).is_zero() && "Should be!");
+  CHECK0(v.cross_product(v).is_zero());
+  CHECK0(vi.cross_product(vi).is_zero());
+  CHECK0(vj.cross_product(vj).is_zero());
+  CHECK0(vk.cross_product(vk).is_zero());
 
-  assert(vi.cross_product(vj) == vk);
-  assert(vj.cross_product(vk) == vi);
-  assert(vk.cross_product(vi) == vj);
+  CHECK0(vi.cross_product(vj) == vk);
+  CHECK0(vj.cross_product(vk) == vi);
+  CHECK0(vk.cross_product(vi) == vj);
 }
 
 static void test_intersection() {
@@ -26,19 +41,19 @@ static void test_intersection() {
   {
     Ray r0 = Ray::from_two_points(Vector(), Vector(1.0, 1.0, 0.0));
     Ray r1 = Ray::from_two_points(Vector(0.0, 1.0, 0.0), Vector(1.0, 0.0, 0.0));
-    assert(r0.intersect(r1, x, y) && "Supposed to intersect!");
+    CHECK(r0.intersect(r1, x, y), "Supposed to intersect!");
   }
 
   {
     Ray r0 = Ray::from_two_points(Vector(), Vector(1.0, 1.0, 0.0));
     Ray r1 = Ray::from_two_points(Vector(), Vector(1.0, 1.0, 0.0));
-    assert(r0.intersect(r1, x, y) && "Supposed to intersect!");
+    CHECK(r0.intersect(r1, x, y), "Supposed to intersect!");
   }
 
   {
     Ray r0 = Ray::from_two_points(Vector(),        Vector(1.0, 1.0, 0.0));
     Ray r1 = Ray::from_two_points(Vector::get_i(), Vector(1.0, 1.0, 1.0));
-    assert(!r0.intersect(r1, x, y) && "Not supposed to intersect!");
+    CHECK(!r0.intersect(r1, x, y), "Not supposed to intersect!");
   }
 }
 
