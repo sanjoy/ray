5760989b7767175d45fcd654660a80052736564c
Sanjoy Das <sanjoy@playingwithpointers.com>
2015-10-18 00:12:42 -0700

    add some scaling

--- src/lib/camera.cpp
+++ src/lib/camera.cpp
@@ -12,12 +12,20 @@ Camera::Camera(const Scene &s, double focal_length, unsigned screen_width_px,
 Bitmap Camera::snap() {
   Bitmap bmp(_screen_height_px, _screen_width_px, Color::create_blue());
 
+  double max_diag_square =
+    std::pow(_screen_height_px / 2, 2) + std::pow(_screen_width_px / 2, 2);
+
+  auto scale = [max_diag_square](int x, int y) {
+    return 1.0 + (x * x + y * y) / max_diag_square;
+  };
+
   for (int xi = -(_screen_width_px / 2), xe = (_screen_width_px / 2); xi != xe;
        ++xi)
     for (int yi = -(_screen_height_px / 2), ye = (_screen_height_px / 2);
          yi != ye; ++yi) {
-      const Vector sample_pt(_focal_length, (xi * 1.0) / _screen_resolution,
-                             (yi * 1.0) / _screen_resolution);
+      double s = scale(xi, yi);
+      const Vector sample_pt(_focal_length, (xi * s) / _screen_resolution,
+                             (yi * s) / _screen_resolution);
       Ray r =
           Ray::from_two_points(_focus_position, _focus_position + sample_pt);
       Color c = _scene.render_pixel(r);
