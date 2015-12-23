1bbbbf80b0b7e61476e9294397feaf50929f69bd
Sanjoy Das <sanjoy@playingwithpointers.com>
2015-12-22 23:49:58 -0800

    Cleanup scene generators

--- src/include/scene-generators.hpp
+++ src/include/scene-generators.hpp
@@ -1,7 +1,8 @@
+/// scene-generators.hpp: predefined scene generators.
+///
+
 #ifndef RAY_SCENE_GENERATORS_HPP
 #define RAY_SCENE_GENERATORS_HPP
-
-/// scene-generators.hpp: predefined scene generators.
 
 #include "scene.hpp"
 
@@ -10,10 +11,16 @@
 namespace ray {
 
 typedef std::function<Camera(Scene &)> SceneGeneratorTy;
-SceneGeneratorTy get_scene_generator_by_name(const char *);
+
+/// Returns the scene generator named \p name, and null if no such
+/// scene generator exists.
+SceneGeneratorTy get_scene_generator_by_name(const char *name);
 
 typedef std::function<void(const char *, SceneGeneratorTy)> SceneGenCallbackTy;
-void for_each_scene_generator(SceneGenCallbackTy);
+
+/// Calls \p callback on each scene generator name & scene generator
+/// pair.
+void for_each_scene_generator(SceneGenCallbackTy callback);
 }
 
 #endif
