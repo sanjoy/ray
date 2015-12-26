26be4ae7c9661b66f581660da9cf128c3093f746
Sanjoy Das <sanjoy@playingwithpointers.com>
2015-12-26 09:18:20 +0100

    Cleanup support.hpp

--- src/include/support.hpp
+++ src/include/support.hpp
@@ -1,39 +1,26 @@
+/// support.hpp: Random helpers that don't belong anywhere else.
+
 #ifndef RAY_SUPPORT_HPP
 #define RAY_SUPPORT_HPP
-
-/// support.hpp: random helpers that don't belong anywhere else.
 
 #include <memory>
 #include <utility>
 
 namespace ray {
 
+/// Abort the program after displaying \p msg.
 void unreachable(const char *msg);
 
+/// Port of std::make_unique from C++14
 template <typename T, typename... Args>
 std::unique_ptr<T> make_unique(Args &&... args) {
   return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
 }
 
-#define TRACE(msg)                                                             \
-  do {                                                                         \
-    if (ENABLE_TRACE)                                                          \
-      printf("trace(%d): %s\n", __LINE__, msg);                                \
-  } while (false)
-
-#define ETRACE(msg)                                                            \
-  do {                                                                         \
-    printf("trace(%d): %s\n", __LINE__, msg);                                  \
-  } while (false)
-
-#define TRACE_ACTION(action)                                                   \
-  do {                                                                         \
-    if (ENABLE_TRACE) {                                                        \
-      action;                                                                  \
-    }                                                                          \
-  } while (false)
-
+/// A wrapper around printf that appends a "\n" after the message
+/// printf would've printed.
 #define printf_cr(fmt, ...) printf(fmt "\n", ##__VA_ARGS__)
+
 }
 
 #endif
