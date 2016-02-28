168b375b7f46b2bdad528e52a249b3875851c28e
Sanjoy Das <sanjoy@playingwithpointers.com>
2016-02-28 12:02:09 -0800

    Overhaul test infra: phase 0

--- src/include/test.hpp
+++ src/include/test.hpp
@@ -9,6 +9,25 @@
 
 namespace ray {
 
+class TestProperties {
+  unsigned _max_failures = 0;
+  std::string _run_only;
+  std::string _exec_name;
+
+public:
+  void set_max_failures(unsigned mf) { _max_failures = mf; }
+  unsigned max_failures() const { return _max_failures; }
+
+  void set_run_only(std::string s) { _run_only = std::move(s); }
+  const std::string &run_only() const { return _run_only; }
+
+  void set_exec_name(std::string s) { _exec_name = std::move(s); }
+  const std::string &exec_name() const { return _exec_name; }
+
+  static TestProperties parse_from_args(int argc, char **argv, bool &success);
+};
+
+namespace internal {
 class TestDecl {
   unsigned _failed_count = 0;
   unsigned _passed_count = 0;
@@ -51,7 +70,6 @@ const MaybeStdErr &operator<<(const MaybeStdErr &out, const T &val) {
   return out;
 };
 
-namespace internal {
 MaybeStdErr check_helper(bool pred, const char *pred_str, const char *msg,
                          const char *file_name, unsigned line_num,
                          TestDecl &test_decl);
@@ -66,6 +84,6 @@ MaybeStdErr check_helper(bool pred, const char *pred_str, const char *msg,
   ray::internal::check_helper(pred, #pred, "", __FILE__, __LINE__,             \
                               __current_test_decl)
 
-#define RAY_INIT_TEST(name) TestDecl __current_test_decl(name);
+#define RAY_INIT_TEST(name) ray::internal::TestDecl __current_test_decl(name);
 
 #endif
--- src/lib/test.cpp
+++ src/lib/test.cpp
@@ -18,3 +18,54 @@ MaybeStdErr ray::internal::check_helper(bool pred, const char *pred_str,
   test_decl.mark_as_failed();
   return MaybeStdErr(true);
 }
+
+TestProperties TestProperties::parse_from_args(int argc, char **argv,
+                                               bool &success) {
+  TestProperties props;
+
+  props.set_exec_name(argv[0]);
+  argc--;
+  argv++;
+
+  bool found_non_pos_arg = false;
+
+  while (argc != 0) {
+    char *current = argv[0];
+    argc--;
+    argv++;
+
+    if (current[0] == '-' && current[1] == '-') {
+      if (!strcmp(current, "--max-failures")) {
+        if (argc == 0) {
+          success = false;
+          return props;
+        }
+
+        char *max_failures_str = argv[0];
+        argc--;
+        argv++;
+
+        char *endptr;
+        long val = strtol(max_failures_str, &endptr, 10);
+        if (endptr != &max_failures_str[strlen(max_failures_str)] || val < 1) {
+          success = false;
+          return props;
+        }
+        props.set_max_failures(val);
+      } else {
+        success = false;
+        return props;
+      }
+    } else {
+      if (found_non_pos_arg) {
+        success = false;
+        return props;
+      }
+      props.set_run_only(current);
+      found_non_pos_arg = true;
+    }
+  }
+
+  success = true;
+  return props;
+}
