2b07687de9d385f6c07c9180b32c3d8b366ddcd3
Sanjoy Das <sanjoy@playingwithpointers.com>
2016-04-09 16:56:13 -0700

    port all tests to gtest

--- src/tests/test-euclid.cpp
+++ src/tests/test-euclid.cpp
@@ -2,178 +2,158 @@
 #include "support.hpp"
 #include "test.hpp"
 
+#include "gtest/gtest.h"
+
 #include <cmath>
 #include <fstream>
 #include <iostream>
 
 using namespace ray;
 
-static const unsigned kCreateVectMax = 8;
-static Vector create_vect(unsigned seed) {
-  switch (seed % kCreateVectMax) {
-  case 0:
-    return Vector(1.3, 3.3, 9.5);
-  case 1:
-    return Vector(100.3, 100.3, 100.5);
-  case 2:
-    return Vector(-16, 333.7, -9.5);
-  case 3:
-    return Vector(-1.3, 0.0, -9.5);
-  case 4:
-    return Vector::get_i();
-  case 5:
-    return Vector::get_j();
-  case 6:
-    return Vector::get_k();
-  case 7:
-    return Vector::get_i() + Vector::get_j() + Vector::get_k();
-  default:
-    unreachable("All cases covered!");
+namespace {
+class EuclidFixture : public ::testing::Test {
+protected:
+  std::vector<Vector> _vectors;
+  std::vector<Plane> _planes;
+
+  EuclidFixture() {
+    _vectors.emplace_back(1.3, 3.3, 9.5);
+    _vectors.emplace_back(100.3, 100.3, 100.5);
+    _vectors.emplace_back(-16, 333.7, -9.5);
+    _vectors.emplace_back(-1.3, 0.0, -9.5);
+    _vectors.push_back(Vector::get_i());
+    _vectors.push_back(Vector::get_j());
+    _vectors.push_back(Vector::get_k());
+    _vectors.push_back(Vector::get_i() + Vector::get_j() + Vector::get_k());
+
+    for (auto &x : _vectors)
+      for (auto &y : _vectors)
+        _planes.emplace_back(x.normalize(), y);
   }
+
+  static Vector change_dir(const Vector &v) {
+    if (v == Vector::get_j())
+      return (v - Vector::get_i()).normalize();
+    return (v - Vector::get_j()).normalize();
+  };
+};
 }
 
-static void test_vector_properties() {
-  RAY_INIT_TEST("Vector properties");
-
+TEST_F(EuclidFixture, basic_vector_properties) {
   Vector vi = Vector::get_i();
   Vector vj = Vector::get_j();
   Vector vk = Vector::get_k();
 
-  CHECK0(vi.cross_product(vi).is_zero());
-  CHECK0(vj.cross_product(vj).is_zero());
-  CHECK0(vk.cross_product(vk).is_zero());
+  EXPECT_TRUE(vi.cross_product(vi).is_zero());
+  EXPECT_TRUE(vj.cross_product(vj).is_zero());
+  EXPECT_TRUE(vk.cross_product(vk).is_zero());
 
-  CHECK0(vi.cross_product(vj) == vk);
-  CHECK0(vj.cross_product(vk) == vi);
-  CHECK0(vk.cross_product(vi) == vj);
+  EXPECT_TRUE(vi.cross_product(vj) == vk);
+  EXPECT_TRUE(vj.cross_product(vk) == vi);
+  EXPECT_TRUE(vk.cross_product(vi) == vj);
 
-  for (unsigned i = 0; i < kCreateVectMax; i++)
-    CHECK0(create_vect(i).cross_product(create_vect(i)).is_zero());
+  for (auto &v : _vectors)
+    EXPECT_TRUE(v.cross_product(v).is_zero());
 
-  for (unsigned i = 0; i < kCreateVectMax; i++) {
-    for (unsigned j = 0; j < kCreateVectMax; j++) {
-      Vector v = create_vect(i);
-      Vector r = create_vect(j);
+  for (auto &v : _vectors)
+    for (auto &r : _vectors)
       if (Ruler::is_zero(v * r))
-        CHECK0(Ruler::is_zero(v.dot_product(v.rotate(M_PI / 2, r))));
-    }
-  }
+        EXPECT_TRUE(Ruler::is_zero(v.dot_product(v.rotate(M_PI / 2, r))));
 
-  for (unsigned i = 0; i < kCreateVectMax; i++) {
-    for (unsigned j = 0; j < kCreateVectMax; j++) {
-      Vector v = create_vect(i);
-      Vector r = create_vect(j);
+  for (auto &v : _vectors)
+    for (auto &r : _vectors)
       if (Ruler::is_zero(v * r))
-        CHECK0(v.cross_product(v.rotate(M_PI, r)).is_zero()) << r << "\n";
-    }
-  }
+        EXPECT_TRUE(v.cross_product(v.rotate(M_PI, r)).is_zero()) << r << "\n";
 
-  for (unsigned i = 0; i < kCreateVectMax; i++) {
-    for (unsigned j = 0; j < kCreateVectMax; j++) {
-      Vector v = create_vect(i);
-      Vector r = create_vect(j);
+  for (auto &v : _vectors)
+    for (auto &r : _vectors)
       if (Ruler::is_zero(v * r))
-        CHECK0(v.cross_product(v.rotate(M_PI, r)).is_zero());
-    }
-  }
+        EXPECT_TRUE(v.cross_product(v.rotate(M_PI, r)).is_zero());
 
-  for (unsigned i = 0; i < kCreateVectMax; i++)
-    for (unsigned j = 0; j < kCreateVectMax; j++) {
-      Vector v = create_vect(i);
-      Vector r = create_vect(j);
-      CHECK0(Ruler::is_equal((v * r), (r * v)));
-    }
+  for (auto &v : _vectors)
+    for (auto &r : _vectors)
+      EXPECT_TRUE(Ruler::is_equal((v * r), (r * v)));
 }
 
-static void test_ray_intersection() {
-  RAY_INIT_TEST("Ray intersections");
-
+TEST_F(EuclidFixture, ray_intersection) {
   Vector origin(0.0, 0.0, 0.0);
   double x, y;
 
   {
     Ray r0 = Ray::from_two_points(origin, Vector(1.0, 1.0, 0.0));
     Ray r1 = Ray::from_two_points(Vector(0.0, 1.0, 0.0), Vector(1.0, 0.0, 0.0));
-    CHECK(r0.intersect(r1, x, y), "Supposed to intersect!");
+    EXPECT_TRUE(r0.intersect(r1, x, y));
   }
 
   {
     Ray r0 = Ray::from_two_points(origin, Vector(1.0, 1.0, 0.0));
     Ray r1 = Ray::from_two_points(origin, Vector(1.0, 1.0, 0.0));
-    CHECK(r0.intersect(r1, x, y), "Supposed to intersect!");
+    EXPECT_TRUE(r0.intersect(r1, x, y));
   }
 
   {
     Ray r0 = Ray::from_two_points(origin, Vector(1.0, 1.0, 0.0));
     Ray r1 = Ray::from_two_points(Vector::get_i(), Vector(1.0, 1.0, 1.0));
-    CHECK(!r0.intersect(r1, x, y), "Not supposed to intersect!");
+    EXPECT_FALSE(r0.intersect(r1, x, y));
   }
 }
 
-static Vector ChangeDir(const Vector &v) {
-  if (v == Vector::get_j())
-    return (v - Vector::get_i()).normalize();
-  return (v - Vector::get_j()).normalize();
-};
-
-static void test_plane_intersection() {
-  RAY_INIT_TEST("Plane intersections");
+TEST_F(EuclidFixture, plane_intersection) {
   // Basic ray-plane intersections
 
   auto CheckIntersection = [&](const Plane &p, const Vector &from,
                                const Vector &to) {
     Ruler::Real k;
     Ray r = Ray::from_two_points(Vector::get_origin(), to);
-    CHECK0(p.intersect(r, k));
-    CHECK0(p.contains(r.at(k)));
+    EXPECT_TRUE(p.intersect(r, k));
+    EXPECT_TRUE(p.contains(r.at(k)));
   };
 
   CheckIntersection(Plane::get_xy(), Vector::get_origin(), Vector::get_k());
   CheckIntersection(Plane::get_xy(), Vector::get_origin() + Vector::get_i(),
                     Vector::get_k() + Vector::get_j());
 
-  for (unsigned i = 0; i < kCreateVectMax; i++) {
-    for (unsigned j = 0; j < kCreateVectMax; j++) {
-      Plane p(create_vect(i).normalize(), create_vect(j));
-      Vector parallel_dir =
-          p.normal().cross_product(ChangeDir(p.normal())).normalize();
-      Vector non_parallel_dir = (parallel_dir + p.normal()).normalize();
+  unsigned vect_index = 0;
 
-      assert(Ruler::is_zero(parallel_dir * p.normal()));
-      assert(!Ruler::is_zero(non_parallel_dir * p.normal()));
+  for (auto &p : _planes) {
+    Vector parallel_dir =
+        p.normal().cross_product(change_dir(p.normal())).normalize();
+    Vector non_parallel_dir = (parallel_dir + p.normal()).normalize();
 
-      Vector init_point = create_vect((i + j) % kCreateVectMax);
-      Ray parallel_ray =
-          Ray::from_offset_and_direction(init_point, parallel_dir);
-      Ray non_parallel_ray =
-          Ray::from_offset_and_direction(init_point, non_parallel_dir);
+    EXPECT_TRUE(Ruler::is_zero(parallel_dir * p.normal()));
+    EXPECT_FALSE(Ruler::is_zero(non_parallel_dir * p.normal()));
 
-      Ruler::Real k;
-      CHECK0(p.intersect(non_parallel_ray, k)) << p << " " << non_parallel_ray
-                                               << "\n";
-      CHECK0(p.contains(non_parallel_ray.at(k)));
-      CHECK0(!p.intersect(parallel_ray, k));
-    }
+    Vector init_point = _vectors[vect_index];
+    vect_index = (vect_index + 1) % _vectors.size();
+
+    Ray parallel_ray = Ray::from_offset_and_direction(init_point, parallel_dir);
+    Ray non_parallel_ray =
+        Ray::from_offset_and_direction(init_point, non_parallel_dir);
+
+    Ruler::Real k;
+    EXPECT_TRUE(p.intersect(non_parallel_ray, k)) << p << " "
+                                                  << non_parallel_ray << "\n";
+    EXPECT_TRUE(p.contains(non_parallel_ray.at(k)));
+    EXPECT_TRUE(!p.intersect(parallel_ray, k));
   }
 }
 
-static void test_rps_intersection() {
-  RAY_INIT_TEST("RectanglePlaneSegment intersections");
-
-  for (unsigned i = 0; i < kCreateVectMax; i++) {
-    for (unsigned j = 0; j < kCreateVectMax; j++) {
-      if (i == j)
+TEST_F(EuclidFixture, rectangle_plane_segment_intersection) {
+  for (auto &pt0 : _vectors) {
+    for (auto &pt1 : _vectors) {
+      if (&pt0 == &pt1)
         continue;
+
+      // The scheme:
       //
       //   pt0 <--- pt1
       //             |
       //             |
       //             V
       //            pt2
-      Vector pt0 = create_vect(i);
-      Vector pt1 = create_vect(j);
+
       Vector dir = pt0 - pt1;
-      Vector dir_perp = dir.cross_product(ChangeDir(dir)).normalize();
+      Vector dir_perp = dir.cross_product(change_dir(dir)).normalize();
       Vector pt2 = pt1 + dir_perp;
 
       RectanglePlaneSegment rps({{pt0, pt1, pt2}});
@@ -184,16 +164,9 @@ static void test_rps_intersection() {
           Ray r = Ray::from_two_points(Vector::get_origin(),
                                        pt1 + x * dir + y * dir_perp);
           Ruler::Real k;
-          CHECK0(rps.intersect(r, k)) << r << " " << rps << "\n";
-          CHECK0(rps.container().contains(r.at(k)));
+          EXPECT_TRUE(rps.intersect(r, k)) << r << " " << rps << "\n";
+          EXPECT_TRUE(rps.container().contains(r.at(k)));
         }
     }
   }
-}
-
-void foo() {
-  test_vector_properties();
-  test_ray_intersection();
-  test_plane_intersection();
-  test_rps_intersection();
 }
